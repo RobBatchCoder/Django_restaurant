53e25911c651a6a80eaf2e071aac20eed36b4370
RobBatchCoder <robertbatchelor909@gmail.com>
2023-03-27 09:25:49 +0000

    add category to admin.py

--- coderscafe/customer/views.py
+++ coderscafe/customer/views.py
@@ -12,3 +12,11 @@ class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
 
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        #get every item from each catagory
+        pass
+        #pass into context
+
+        #render the template
+
